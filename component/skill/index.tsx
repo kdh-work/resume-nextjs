00ed7721816b714d3df527baf9d7782ec3b98c01
dahye_kwon <dh.kwon@okestro.com>
2025-01-02 23:10:09 +0900

    feat: profile, skills 영역 까지 우선 작업

--- component/skill/index.tsx
+++ component/skill/index.tsx
@@ -1,7 +1,5 @@
-import { Row, Col, Tooltip } from 'reactstrap';
-import { PropsWithChildren, useState } from 'react';
-import { faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Row, Col } from 'reactstrap';
+import { PropsWithChildren } from 'react';
 import { Style } from '../common/Style';
 import { ISkill } from './ISkill';
 import SkillRow from './row';
@@ -27,7 +25,6 @@ function Component({ payload }: PropsWithChildren<{ payload: Payload }>) {
           <Col>
             <h2>
               <span style={Style.blue}>SKILL</span>
-              {createTooltip(payload.tooltip)}
             </h2>
           </Col>
         </Row>
@@ -36,30 +33,5 @@ function Component({ payload }: PropsWithChildren<{ payload: Payload }>) {
         ))}
       </EmptyRowCol>
     </div>
-  );
-}
-
-function createTooltip(content?: string) {
-  if (!content) {
-    return '';
-  }
-
-  const [tooltipOpen, setTooltipOpen] = useState(false);
-  const toggle = () => setTooltipOpen(!tooltipOpen);
-
-  return (
-    <small>
-      {' '}
-      <FontAwesomeIcon icon={faQuestionCircle} id="skill-tooltip" />
-      <Tooltip
-        style={{ whiteSpace: 'pre-wrap' }}
-        placement="right"
-        target="skill-tooltip"
-        isOpen={tooltipOpen}
-        toggle={toggle}
-      >
-        {content}
-      </Tooltip>
-    </small>
   );
 }
