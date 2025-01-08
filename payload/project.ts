23d98a6aaf43300c13ecf25ead294ad42cdb8097
dahye_kwon <dh.kwon@okestro.com>
2025-01-08 23:32:13 +0900

    feat: project, education 내용 업데이트 및 사용하지 않는 컴포넌트 주석처리

--- payload/project.ts
+++ payload/project.ts
@@ -4,38 +4,93 @@ const project: IProject.Payload = {
   disable: false,
   list: [
     {
-      title: 'Linux Kernel Optimization Project',
-      startedAt: '2020-06',
-      where: 'Linux Foundation',
+      title: '하나금융 TI 통합 모니터링 솔루션 공동 개발 프로젝트 2차',
+      startedAt: '2025-01',
+      where: '오케스트로',
       descriptions: [
         {
-          content:
-            'Initiated and lead a collaborative project aimed at optimizing the Linux Kernel for various hardware architectures.',
+          content: '프론트엔드 개발 및 고도화',
         },
         {
-          content:
-            'Achieved significant improvements in system performance and resource management.',
+          content: '고도화2',
           weight: 'MEDIUM',
-          descriptions: [
-            { content: '30% improvement in system resource efficiency' },
-            { content: 'Job Scheduler Refactor and Optimization' },
-          ],
+          descriptions: [{ content: '설명1' }, { content: '설명2' }],
         },
       ],
     },
     {
-      title: 'Global Linux Bootcamp',
-      startedAt: '2017-05',
-      endedAt: '2019-12',
-      where: 'Open Source Community',
+      title: '하나금융 TI 통합 모니터링 솔루션 공동 개발 프로젝트 1차',
+      startedAt: '2023-07',
+      endedAt: '2024-12',
+      where: '오케스트로',
       descriptions: [
         {
-          content:
-            'Organized and conducted training bootcamps focusing on Linux system engineering skills.',
+          content: '프론트엔드 개발',
         },
         {
-          content:
-            'Successfully trained over 5,000 participants in system administration and security. ',
+          content: '고도화2',
+          weight: 'MEDIUM',
+          descriptions: [{ content: '설명1' }, { content: '설명2' }],
+        },
+      ],
+    },
+    {
+      title: 'KB 국민은행 IQ+ 시스템 구축 프로젝트',
+      startedAt: '2023-10',
+      endedAt: '2024-01',
+      where: 'KB 국민은행',
+      descriptions: [
+        {
+          content: '고도화1',
+        },
+        {
+          content: '고도화2',
+          weight: 'MEDIUM',
+          descriptions: [{ content: '설명1' }, { content: '설명2' }],
+        },
+      ],
+    },
+    {
+      title: '자사 통합 모니터링 솔루션 POC',
+      startedAt: '2023-05',
+      endedAt: '2023-07',
+      where: '오케스트로',
+      descriptions: [
+        {
+          content: '고도화1',
+        },
+        {
+          content: '고도화2',
+          weight: 'MEDIUM',
+          descriptions: [{ content: '설명1' }, { content: '설명2' }],
+        },
+      ],
+    },
+    {
+      title: '삼성 SCP OpenStack 플랫폼 구축 프로젝트',
+      startedAt: '2022-07',
+      endedAt: '2023-04',
+      where: '삼성 SDS',
+      descriptions: [
+        {
+          content: '개발1',
+        },
+        {
+          content: '개발2',
+        },
+      ],
+    },
+    {
+      title: '건설공제 고객 중심 정보시스템 고도화 사업',
+      startedAt: '2022-02',
+      endedAt: '2022-06',
+      where: '건설공제조합',
+      descriptions: [
+        {
+          content: '개발1',
+        },
+        {
+          content: '개발2',
         },
       ],
     },
